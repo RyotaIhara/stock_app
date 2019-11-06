d2748647afff00a4e43bb2b18f967d685a4fc285
program <ihara0531@gmail.com>
2019-11-06 19:41:33 +0900

    drop member

--- db/migrate/20191105111910_remove_password_to_member.rb
+++
@@ -1,5 +0,0 @@
-class RemovePasswordToMember < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :members, :password, :string
-  end
-end
--- db/migrate/20191105112813_add_password_digest_to_member.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordDigestToMember < ActiveRecord::Migration[6.0]
-  def change
-    add_column :members, :password_digest, :string
-  end
-end
